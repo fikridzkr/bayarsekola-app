611b3abb43ec25c2810bf903e7dc751e519cc92a
fikridzkr <fikridzakir179@gmail.com>
2021-04-06 06:39:28 +0700

    display data operatorin dashboard operator

--- src/components/admin/DataOperators.js
+++ src/components/admin/DataOperators.js
@@ -1,7 +1,55 @@
-import React from "react";
-
+import React, { useEffect, useState } from "react";
+import { Button, Table, Badge } from "react-bootstrap";
+import Axios from "axios";
 const DataOperators = () => {
-  return <div>data</div>;
+  const [dataOperators, setDataOperators] = useState([]);
+  useEffect(() => {
+    Axios.get("http://localhost:3001/admin/dataoperators")
+      .then((res) => {
+        setDataOperators(res.data.operators);
+      })
+      .catch((err) => console.log(err));
+  });
+  return (
+    <div className="m-5">
+      <h2>Data Admin BayarSekola</h2>
+      <hr />
+      <Button variant="outline-success" className="mb-3 float-right">
+        Tambah Data
+      </Button>
+      <Table striped bordered hover responsive>
+        <thead className="bg-success text-white">
+          <tr>
+            <th>No</th>
+            <th>Username</th>
+            <th>Email</th>
+            <th>Status</th>
+            <th>Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          {dataOperators.map((values) => {
+            return (
+              <tr>
+                <td>1</td>
+                <td>{values.username}</td>
+                <td>{values.email}</td>
+                <td>
+                  <Badge variant="success">{values.is_active}</Badge>
+                </td>
+                <td>
+                  {" "}
+                  <Button variant="danger" size="sm">
+                    Delete
+                  </Button>
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </Table>
+    </div>
+  );
 };
 
 export default DataOperators;
