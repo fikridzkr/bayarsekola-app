ef1333668066679e5eae5fc8b2aeb6b95808557a
fikridzkr <fikridzakir179@gmail.com>
2021-04-12 15:05:57 +0700

    create feature change password

--- src/components/admin/ProfileSettings.js
+++ src/components/admin/ProfileSettings.js
@@ -1,26 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
 import { Container, Form, Button, Col, Row } from "react-bootstrap";
+import Axios from "axios";
+import swal from "sweetalert";
 
-const ProfileSettings = () => {
+const ProfileSettings = ({ user_id }) => {
+  const idUser = user_id;
+  const [newPassword, setNewPassword] = useState();
+  const changePassword = () => {
+    Axios.put("http://localhost:3001/changepassword", {
+      user_id: idUser,
+      newPassword: newPassword,
+    })
+      .then(handleSubmit())
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+  const handleSubmit = () => {
+    swal("Yeyyy!", "Your password has been changed successfully!", "success", {
+      button: "Okay",
+    }).then(() => {
+      setNewPassword("");
+    });
+  };
   return (
     <>
       <Container className="mt-3">
-        <h1>Profile Settings</h1>
+        <h3>Profile Settings</h3>
         <hr />
         <h4 className="mb-3">Change Password</h4>
         <Row>
-          <Col md={6}>
+          <Col md={8} sm={12} xs={12}>
             <Form>
               <Form.Group>
                 <Form.Control
                   type="password"
                   placeholder="Enter new password"
+                  className="col-md-6"
+                  onChange={(event) => setNewPassword(event.target.value)}
                 />
                 <Form.Text className="text-muted">
                   We'll never share your password with anyone else.
                 </Form.Text>
               </Form.Group>
-              <Button variant="danger" type="submit">
+              <Button variant="danger" onClick={() => changePassword()}>
                 Change Password
               </Button>
             </Form>
