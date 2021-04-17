57875ef1b8a572fbe8ec8f39751419790d77cef8
fikridzkr <fikridzakir179@gmail.com>
2021-04-17 11:24:07 +0700

    Fix bug:

--- src/admin/index.js
+++ src/admin/index.js
@@ -3,7 +3,6 @@ import Navigation from "../components/admin/Navigation";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import DataStudents from "../components/admin/DataStudents";
 import DataAdmin from "../components/admin/DataAdmin";
-import DataPayment from "../components/admin/DataPayment";
 import DataOperators from "../components/admin/DataOperators";
 import ProfileSettings from "../components/admin/ProfileSettings";
 import Home from "../components/admin/Home";
